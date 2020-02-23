6313b57e4d5d6812c4da6371f566e5d12aa99ab1
a.efimova <asi4ka.84@gmail.com>
2020-02-24 00:09:51 +0300

    add api

---
+++ api.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:7070/api';
+
+interface IServicesList {
+    id: string,
+    name: string,
+    price: number
+}
+interface IServicesItem {
+    id: string,
+    name: string,
+    price: number,
+    content: string
+}
+
+export const fetchList = ():Promise<IServicesList[]> => axios.get('/services', { 'headers': {'Content-Type': 'application/json'}}).then(response => response.data);
+
+export const fetchItem = (id:string):Promise<IServicesItem> => axios.get(`/services/${id}`, { 'headers': {'Content-Type': 'application/json'}}).then(response => response.data);
